bd0313e4a758e7e50f40da4bc9bc08d0251fbe79
zhangleilei <zhangleilei@metasota.ai>
2020-06-29 11:40:54 +0800

    add highlight in preview mode

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -6,8 +6,9 @@
 </template>
 
 <script>
-    import marked from 'marked'
-    import hljs from 'highlight.js'
+    import marked from 'marked';
+    import hljs from 'highlight.js';
+    import 'highlight.js/styles/github.css';
     export default {
         name: "Editor",
         data: function(){
@@ -18,7 +19,9 @@
         computed: {
             compiledMarkdown: function() {
                 let markdown = marked(this.input, { sanitize: true })
-                //return hljs.highlightAuto(markdown).value;
+                setTimeout(()=>{
+                    this.highlightCode()
+                }, 0)
                 return markdown
             }
         },
@@ -30,8 +33,9 @@
             },
             highlightCode: function () {
                 let pres = document.querySelectorAll("pre>code");
-                for (let i = 0; i < pres.length; i++) {
-                }
+                pres.forEach((block) => {
+                    hljs.highlightBlock(block);
+                });
             }
         }
     }
@@ -72,3 +76,49 @@
         color: white;
     }
 </style>
+
+
+<!--# (GitHub-Flavored) Markdown Editor-->
+
+<!--This Editor contributed by jbt on GitHub: <https://github.com/jbt/markdown-editor>-->
+
+<!--Basic useful feature list:-->
+
+<!--* Ctrl+S / Cmd+S to save the file-->
+<!--* Ctrl+Shift+S / Cmd+Shift+S to choose to save as Markdown or HTML-->
+<!--* Drag and drop a file into here to load it-->
+<!--* File contents are saved in the URL so you can share files-->
+
+
+<!--I'm no good at writing sample / filler text, so go write something yourself.-->
+
+<!--Look, a list!-->
+
+<!--* foo-->
+<!--* bar-->
+<!--* baz-->
+
+<!--And here's some code! :+1:-->
+
+<!--```javascript-->
+<!--$(function(){-->
+<!--$('div').html('I am a div.');-->
+<!--});-->
+<!--```-->
+
+<!--This is [on GitHub](https://github.com/jbt/markdown-editor) so let me know if I've borked it somewhere.-->
+
+
+<!--Props to Mr. Doob and his [code editor](http://mrdoob.com/projects/code-editor/), from which-->
+<!--the inspiration to this, and some handy implementation hints, came.-->
+
+<!--### Stuff used to make this:-->
+
+<!--* [markdown-it](https://github.com/markdown-it/markdown-it) for Markdown parsing-->
+<!--* [CodeMirror](http://codemirror.net/) for the awesome syntax-highlighted editor-->
+<!--* [highlight.js](http://softwaremaniacs.org/soft/highlight/en/) for syntax highlighting in output code blocks-->
+<!--* [js-deflate](https://github.com/dankogai/js-deflate) for gzipping of data to make it fit in URLs-->
+
+<!--### Links-->
+
+<!--[markdown-plus](https://github.com/tylingsoft/markdown-plus) is a versatile markdown editor.-->
